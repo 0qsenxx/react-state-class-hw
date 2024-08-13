82da493558622835fc3451896f056cf701c103fc
0qsenxx <semen.antipyuk@gmail.com>
2024-08-13 14:44:20 +0300

    Updates

--- src/components/Notification/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Notification.module.css";
-
-const Notification = ({ message }) => (
-  <p className={styles.notificationText}>No feadback given.</p>
-);
-
-export default Notification;
